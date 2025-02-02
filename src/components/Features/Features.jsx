58870616bfdc7b9e022543d7df69e1e7e5e10ea7
rjspyk5 <rjspyk5@gmail.com>
2025-02-02 23:22:06 +0600

    features section complete

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -6,12 +6,12 @@ import meeting from '../../assets/images/meeting.png'
 
 export const Features = () => {
   return (
-    <div>
-      <div className="flex justify-between rounded-2xl p-5 border-[1px] border-[#FFFFFF0D]">
-        <div className="w-1/2">
-          <div className="max-w-md">
+    <div className="lg:mx-[8%] md:mx-[6%] mx-[3%] space-y-5">
+      <div className="flex justify-between rounded-2xl p-6  border-[1px] bg-[#FFFFFF0D] backdrop-blur-3xl border-[#FFFFFF0D]">
+        <div className="w-1/2 flex flex-col items-center justify-center">
+          <div className="max-w-md ">
             <h1 className="text-3xl font-bold">High-Quality Print Solutions</h1>
-            <p className="py-6 text-base">
+            <p className="py-6 text-base text-deem">
             From business cards to banners, Fowzi Prints delivers exceptional results tailored to your needs
             </p>
            <PrimaryButton name="Explore Fowzi Prints"/>
@@ -22,7 +22,7 @@ export const Features = () => {
         <img src={img} alt="" />
         </div>
       </div>
-      <div className="flex flex-col lg:flex-row gap-5">
+      <div className="flex flex-col lg:flex-row gap-4">
         <FeatruesCard title="State-of-the-Art Studio Rentals" subtitle="Fowzi Studios provides professional-grade spaces for all your creative projects." buttonName="Visit Fowzi Studios" img={meeting}/>
         <FeatruesCard title="State-of-the-Art Studio Rentals" subtitle="Fowzi Studios provides professional-grade spaces for all your creative projects." buttonName="Visit Fowzi Studios" img={meeting}/>
       </div>
--- src/components/Features/FeaturesCard/FeatruesCard.jsx
+++ src/components/Features/FeaturesCard/FeatruesCard.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import { PrimaryButton } from '../../Button/PrimaryButton/PrimaryButton'
+import { FaArrowRightLong } from 'react-icons/fa6'
 
 export const FeatruesCard = ({img,title,subtitle,buttonName}) => {
   return (
-      <div className="flex justify-between rounded-2xl p-5 border-[1px] border-[#FFFFFF0D]">
+      <div className="flex justify-between rounded-2xl p-5 border-[1px] bg-[#FFFFFF0D] border-[#FFFFFF0D]">
             <div className="w-1/2">
               <div className="max-w-md">
                 <h1 className="text-2xl font-bold">{title}</h1>
-                <p className="py-6 text-sm">
+                <p className="py-3 text-sm">
                 {subtitle}
                 </p>
-    <PrimaryButton classname={"border border-white"} name={buttonName} />
+    
+     <button className={`cursor-pointer font-bold border  rounded-lg px-[10px] py-[10px] flex items-center gap-2`}>{buttonName} <FaArrowRightLong/></button>
               </div>
             </div>
             <div className="w-1/2">
