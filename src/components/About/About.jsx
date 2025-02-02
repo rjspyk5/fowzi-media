e713fa8323276c03526bfaebc952e0401680b349
rjspyk5 <rjspyk5@gmail.com>
2025-02-02 22:00:19 +0600

    basic about section complete

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import { SectionHeading } from '../SectionHeading/SectionHeading'
+import { PrimaryButton } from '../Button/PrimaryButton/PrimaryButton'
+import lookInto from '../../assets/images/lookIntoCamera.png'
+import camera from '../../assets/images/camera.png'
 
 export const About = () => {
   return (
-    <div>About</div>
+    <div className='flex'>
+        <div className="w-1/2 flex gap-7">
+        <div className="">
+            <img src={lookInto} alt="" />
+        </div>
+        <div className="">
+            <img className='mb-10' src={camera} alt="" />
+           <PrimaryButton classname={"border border-[white]"} name={"Learn more"} />
+        </div>
+        
+        </div>
+        <div className="w-1/2 flex justify-end items-center">
+        <SectionHeading first={"About"} second={"Fowzi Media"} button={"About us"} subheading={"We bring together digital, print, and studio services to help your business grow with an integrated approach. Our experienced team delivers high-quality results tailored to your brand’s needs.'- Visual: A single, clean icon or graphic that represents collaboration, such as three intersecting lines or shapes, symbolizing the three sub-brands."} />
+        </div>
+    </div>
   )
 }
---
+++ src/components/SectionHeading/SectionHeading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const SectionHeading = ({first,second,button,className,subheading}) => {
+  return (
+    <div className={`${className && className} max-w-md`}>
+        <button className='rounded-[20px] font-bold bg-[#FFFFFF36] mb-5 px-5 py-[6px]'>{button}</button>
+        <h2 className='text-4xl mb-4 font-bold'>{first} + <span className='text-primary-1'>{second}</span></h2>
+        <p>{subheading}</p>
+    </div>
+  )
+}
